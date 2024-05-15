2ba1f9e413a54223b3d6cf795d645d24a8a798c1
Isha <ishadheke4@gmail.com>
2024-05-15 16:49:38 +0545

    handled error shown in w3c

--- home.js
+++ home.js
@@ -1,8 +1,10 @@
 const toggleButton = document.querySelector('.header__bar-icon');
 const headerMid = document.querySelector('.header__bar-mid ul ');
-
 toggleButton.addEventListener('click',()=>{
     console.log('clicked')
     headerMid.classList.toggle('active');
-   
 });
+
+const changeIcon = function (icon){
+    icon.classList.toggle('fa-times');
+} 
