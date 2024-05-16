8bd2cf38dee2e2210ba380beea822bae62ccbd05
Isha <ishadheke4@gmail.com>
2024-05-16 17:01:22 +0545

    added dropdown, slider, and changed content accordig to different tabs

--- home.js
+++ home.js
@@ -1,5 +1,7 @@
 const toggleButton = document.querySelector('.header__bar-icon');
 const headerMid = document.querySelector('.header__bar-mid ul ');
+
+
 toggleButton.addEventListener('click',()=>{
     console.log('clicked')
     headerMid.classList.toggle('active');
@@ -8,3 +10,43 @@ toggleButton.addEventListener('click',()=>{
 const changeIcon = function (icon){
     icon.classList.toggle('fa-times');
 } 
+
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        items: 1,
+        loop: true,
+        nav: true,  
+        autoplay: true,
+        autoplaySpeed: 1000,
+        autoplayHoverPause: true,
+    });
+})
+
+
+window.onload = function() {
+    document.getElementById('SALE').style.display = "flex";
+  
+    var tabButton = document.getElementsByClassName("tab_button");
+    for (var i = 0; i < tabButton.length; i++) {
+      if (tabButton[i].textContent !== 'SALE') {
+        tabButton[i].classList.remove("active");
+      } else {
+        tabButton[i].classList.add("active");
+      }
+    }
+  }
+
+function MyProduct(evt, productName){
+    let categories__product =document.getElementsByClassName('categories__product');
+    for(let i=0; i<categories__product.length;i++){
+        categories__product[i].style.display='none'
+    }
+
+    let tabButton = document.getElementsByClassName("tab_button");
+  for (i = 0; i < tabButton.length; i++) {
+    tabButton[i].className = tabButton[i].className.replace(" active", "");
+  }
+
+  document.getElementById(productName).style.display = "flex";
+  evt.currentTarget.className += " active";
+}
